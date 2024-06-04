609dd67de0e40a5984e0466a25dfc328ccdef564
noam1222 <noam1222@gmail.com>
2024-06-04 23:27:50 +0300

    add modals warning when delete

--- widgets/library/trash.py
+++ widgets/library/trash.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets, QtGui, QtCore
 from constants import getIconPath
 from db.controller import delete_unique_report, delete_all_reports_by_date_and_time
 
+
 class TrashLabel(QtWidgets.QLabel):
     def __init__(self, parent, libraryWindow):
         super().__init__(parent)
@@ -27,7 +28,10 @@ class TrashLabel(QtWidgets.QLabel):
     def dropEvent(self, event):
         self.setPixmap(QtGui.QPixmap(getIconPath("trash_can_icon.png")))
         if event.source() in (self.libraryWindow.treeWidget_2, self.libraryWindow.treeWidget):
-            dragged_item = self.libraryWindow.treeWidget_2.currentItem()
+            if event.source() ==  self.libraryWindow.treeWidget_2:
+                dragged_item = self.libraryWindow.treeWidget_2.currentItem()
+            else:
+                dragged_item = self.libraryWindow.treeWidget.currentItem()
             if not dragged_item:
                 dragged_item = self.libraryWindow.treeWidget.currentItem()
             text = dragged_item.text(0)
@@ -36,14 +40,19 @@ class TrashLabel(QtWidgets.QLabel):
                 parent_item = dragged_item.parent()
                 if parent_item:
                     parent_text = parent_item.text(0)
+                    date = self.libraryWindow.curr_date
+                    time = text
                     if parent_text == "All":
-                        # TODO check if work after ari update
-                        delete_all_reports_by_date_and_time(self.libraryWindow.curr_date, text)
-                        self.libraryWindow.filter()
+                        reply = self.deleteModal("ALL", date, time)
+                        if reply == QtWidgets.QMessageBox.Ok:
+                            delete_all_reports_by_date_and_time(date, time)
+                            self.libraryWindow.filter()
                     else:
                         app_num = self.libraryWindow.get_app_num(parent_text)
-                        delete_unique_report(self.libraryWindow.curr_date, text, app_num)
-                        self.libraryWindow.filter()
+                        reply = self.deleteModal(f"applysia {app_num}", date, time)
+                        if reply == QtWidgets.QMessageBox.Ok:
+                            delete_unique_report(date, time, app_num)
+                            self.libraryWindow.filter()
             else:
                 event.ignore()
         else:
@@ -52,3 +61,13 @@ class TrashLabel(QtWidgets.QLabel):
     def is_child(self, text):
         return text[0] == "A"
 
+    def deleteModal(self, app, date, time):
+        msg = QtWidgets.QMessageBox()
+        msg.setIcon(QtWidgets.QMessageBox.Warning)
+        msg.setText(f"This operation will delete {app} reports from:\n" +
+                    f"Date: {date}\n" +
+                    f"Time: {time}\n" +
+                    "Are you sure?")
+        msg.setWindowTitle("Warning")
+        msg.setStandardButtons(QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
+        return msg.exec_()
